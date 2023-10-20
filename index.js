bc0d181536f4567baf91cf86cad30a4e3de77332
Reginawambui <ngendoanita8@gmail.com>
2023-10-20 08:12:09 +0300

    completedassingment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+const num1 = 31;
+ const num2 = 2;
+  const multiply = num1 * num2
+
+
+   const random = Math.floor(Math.random() * 10) + 1;
+
+const num3 = 18;
+const num4 = 9
+const mod =18 % 9;
+
+let max = Math.max(12, 8 , 20, 17, 2);
